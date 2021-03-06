262708433bc73eb8e084cbfe22b6eba116fb35f9
Da-bin-kim <omming@naver.com>
2021-03-06 23:17:28 +0900

    왜안대

--- src/main/java/com/dessert/ringring/mapper/BoardMapper.java
+++ src/main/java/com/dessert/ringring/mapper/BoardMapper.java
@@ -9,12 +9,11 @@ import com.dessert.ringring.domain.DTOBoard;
 @Mapper
 public interface BoardMapper {
 	
-	public int insertBoard(DTOBoard board);
-	public DTOBoard selectBoardDetail(int idx);
-	public int updateBoard(DTOBoard board);
-	public int deleteBoard(int idx);
-	public List<DTOBoard> selectBoardList();
-	public int selectBoardTotalCount();
-	
+	public int insertBoard(DTOBoard board); //게시글 생성 쿼리 호출
+	public DTOBoard selectBoardDetail(int idx); //게시글 조회 쿼리 호출
+	public int updateBoard(DTOBoard board); //게시글 수정 쿼리 호출
+	public int deleteBoard(int idx); //게시글 삭제 쿼리 호출
+	public List<DTOBoard> selectBoardList(); //게시글 목록 조회 쿼리 호출
+	public int selectBoardTotalCount(); //삭제여부 N으로 지정된 게시글 조회 쿼리 호출
 
 }
